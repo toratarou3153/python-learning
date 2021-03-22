77682d4f09553e0d83016d4bf628520ed2f3eb84
toratarou3153 <toranosuke3310718s@yahoo.co.jp>
2021-03-22 16:08:43 +0900

    add_abc163_a.py

---
+++ 01/abc163_a.py
@@ -0,0 +1,16 @@
+# ABC163A - Circle Pond
+import math
+# WRITE HERE
+
+def main():
+    # input
+    R = int(input())
+
+    # compute
+
+    # output
+    print(2*R*math.pi)
+
+
+if __name__ == '__main__':
+    main()
